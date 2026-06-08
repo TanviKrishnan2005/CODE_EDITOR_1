000b9571955d15e61ae4722726fee0ebd3108b99
Tanvi <rmtanvikrishnan@gmail.com>
2026-06-08 22:43:00 +0530

    Update backend URL

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://code-editor-2zom.onrender.com";
+  import.meta.env.VITE_API_BASE_URL || "https://code-editor-1-4o1v.onrender.com";
 
 export const socket = io(API_BASE_URL, {
   transports: ["websocket"],
